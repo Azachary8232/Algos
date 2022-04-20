fcb6b2ac99ead94a5d5d8f84af847bf2fd073705
Azachary8232 <azachary8232@hotmail.com>
2022-04-20 11:14:21 -0600

    Add Plus Minus Algo

---
+++ PlusMinus.java
@@ -0,0 +1,18 @@
+
+
+public class PlusMinus{
+
+    class Result {
+
+
+        public static void Minus(List<Integer> arr) {
+        System.out.println("Hello");
+
+        }
+
+    }
+
+
+    public static void main(String[] args){    
+    }
+}
