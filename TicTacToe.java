67ca778ae0fc8fd52d0da0786b8f1731e5b6c51f
Azachary8232 <azachary8232@hotmail.com>
2022-04-20 10:04:20 -0600

    Added TicTacToe

---
+++ TicTacToe.java
@@ -0,0 +1,18 @@
+
+public class Main{
+
+    // 2d Array
+    
+    int[][] grid = new int[10][10];
+
+    public void turn (int x, int y, int player){
+        grid[x][y] = player;
+    }
+
+    public boolean result (grid){
+        
+    }
+
+    public static void main{String[] Args
+    }
+}
