aaf564e3047e73e4194fec063dc25cedd15b1b9e
Azachary8232 <azachary8232@hotmail.com>
2022-05-09 14:42:03 -0600

    Updated

--- lonely_integer.py
+++ lonely_integer.py
@@ -1,5 +1,15 @@
 
 
-def lonelyInteger():
+def lonelyInteger(a):
     new_list = []
-    
+    for i in a:
+        if i in new_list:
+            new_list.remove(i)
+        else:
+            new_list.append(i)
+    return new_list[0]
+
+
+
+print(lonelyInteger([2,3,4,3,2]))
+# print(lonelyInteger([1,1,2]))
