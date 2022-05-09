d680c2091b890f2e26099f2a64dc75bd87c27210
Azachary8232 <azachary8232@hotmail.com>
2022-05-09 16:29:13 -0600

    Updated

--- LonelyInteger.java
+++ LonelyInteger.java
@@ -12,7 +12,22 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 
+class Result{
 
+    public static int lonelyinteger(List<Integer> a){
+        ArrayList<Integer> newArray = new ArrayList<>();
+        for( int i = 0; i < a.size(); i++) {
+            if(newArray.contains(a.get(i))){
+                newArray.remove(a.get(i));
+            }
+            else{
+                newArray.add(a.get(i));
+            }
+        }
+        return newArray.get(0);
+    }
+
+}
 
 
 
@@ -20,9 +35,17 @@ import static java.util.stream.Collectors.toList;
 
 public class LonelyInteger {
     public static void main(String[] args) {
+        ArrayList<Integer> newArry = new ArrayList<>();
+        newArry.add(1);
+        newArry.add(2);
+        newArry.add(3);
+        newArry.add(4);
+        newArry.add(3);
+        newArry.add(2);
+        newArry.add(1);
 
-    
 
+    System.out.println(Result.lonelyinteger(newArry));
     }    
-    
+
 }    
