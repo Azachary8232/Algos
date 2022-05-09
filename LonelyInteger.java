41224566c7ca9c6fe7369a858ad527e22e6ee9b6
Azachary8232 <azachary8232@hotmail.com>
2022-05-09 15:00:20 -0600

    Updated

---
+++ LonelyInteger.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+
+
+
+
+
+public class LonelyInteger {
+    public static void main(String[] args) {
+
+    
+
+    }    
+    
+}    
