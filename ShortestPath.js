f935e527682a1249eb09c7b782afe8872d466f6a
Azachary8232 <azachary8232@hotmail.com>
2022-05-16 19:59:44 -0600

    Updated

---
+++ ShortestPath.js
@@ -0,0 +1,13 @@
+
+
+
+
+function shortestPathBinaryMatrix(grid) {
+    const gridLength = grid.length;
+    console.log(gridLength);
+};
+
+
+
+
+console.log(shortestPathBinaryMatrix([[0,1],[1,0]]))
