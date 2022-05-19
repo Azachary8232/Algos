1e9f417a8b7b80fc9454a3dd9956a97ee06868e9
Azachary8232 <azachary8232@hotmail.com>
2022-05-19 00:03:04 -0600

    Updated

--- linked_lists.js
+++ linked_lists.js
@@ -70,14 +70,46 @@
                         // Recursive Linked List
 
 
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.next = null
+//     }
+// }
+
+
+
+// class LinkedList{
+//     constructor(){
+//         this.head = null
+//     }
+
+//     append(value){
+//         if(this.head === null){
+//             this.head = new Node(value);
+//             return
+//         }
+//         this._append(value, this.head);
+//     }
+
+//     _append(value, current){
+//         if(current.next === null){
+//             current.next = new Node(value);
+//             return;
+//         }
+//         this._append(value, current.next);
+//     }
+// }
+
+
+                        // Singly Linked List Deletion
+
 class Node{
     constructor(value){
         this.value = value;
-        this.next = null
+        this.next = null;
     }
 }
-
-
 
 class LinkedList{
     constructor(){
@@ -89,27 +121,52 @@ class LinkedList{
             this.head = new Node(value);
             return
         }
-        this._append(value, this.head);
+        
+        let current = this.head;
+        while(current.next !== null){
+            current = current.next;
+        }
+        current.next = new Node(value);
     }
 
-    _append(value, current){
-        if(current.next === null){
-            current.next = new Node(value);
-            return;
+    print(){
+        let str = '';
+        let current = this.head
+        if(this.head === null){
+            return str
         }
-        this._append(value, current.next);
+        while(current !== null){
+            str += current.value + " -> ";
+            current = current.next;
+        }
+
+        return str
+    }
+
+    delete(target){
+        let head = this.head
+        if(head.value === target){
+            this.head = head.next;
+            return
+        }
+        let current = head;
+        let previous = null
+        while(current !== null){
+            if(current.value === target){
+                previous.next = current.next;
+                return
+            }
+            previous = current;
+            current = current.next;
+        }
     }
 }
-
-
-
-
-
-
-
 
 const list = new LinkedList();
 list.append('a');
 list.append('b');
 list.append('c');
-console.log(list.head)
+list.append('d');
+list.delete('d')
+// console.log(list.head)
+console.log(list.print());
