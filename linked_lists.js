4019e69a9923da05f3fdf44287ba4c012423eb1e
Azachary8232 <azachary8232@hotmail.com>
2022-05-18 18:14:28 -0600

    Updated

--- linked_lists.js
+++ linked_lists.js
@@ -1,6 +1,75 @@
 
 
 
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.next = null
+//     }
+// }
+
+
+// class LinkedList{
+//     constructor(){
+//         this.head = null;
+//     }
+
+//     append(value){
+//         if (this.head === null){
+//             this.head = new Node(value);
+//             return;
+//         }
+        
+//         let current = this.head;
+//         while (current.next !== null) {
+//             current = current.next;
+//         }
+//         current.next = new Node(value)
+//     }
+
+//     print(){
+//         let str = '';
+//         let current = this.head;
+//         while( current !== null){
+//             str += current.value + ' -> ';
+//             current = current.next;
+//         }
+//         console.log(str);
+//     }
+
+//     contains(target) {
+//         let current = this.head;
+//         while( current !== null){
+//             if (current.value === target){
+//                 return true
+//             }
+//             current = current.next;
+//         }
+//         return false
+//     }
+
+// }
+
+
+// const list = new LinkedList();
+// list.append('a');
+// list.append('b');
+// list.append('c');
+// list.append(true);
+// list.print();
+
+// console.log(list.contains('a'));
+// console.log(list.contains('b'));
+// console.log(list.contains('c'));
+// console.log(list.contains(true));
+// console.log(list.contains('x'));
+// console.log(list.contains('z'));
+
+
+
+                        // Recursive Linked List
+
+
 class Node{
     constructor(value){
         this.value = value;
@@ -9,58 +78,38 @@ class Node{
 }
 
 
+
 class LinkedList{
     constructor(){
-        this.head = null;
+        this.head = null
     }
 
     append(value){
-        if (this.head === null){
+        if(this.head === null){
             this.head = new Node(value);
+            return
+        }
+        this._append(value, this.head);
+    }
+
+    _append(value, current){
+        if(current.next === null){
+            current.next = new Node(value);
             return;
         }
-        
-        let current = this.head;
-        while (current.next !== null) {
-            current = current.next;
-        }
-        current.next = new Node(value)
+        this._append(value, current.next);
     }
-
-    print(){
-        let str = '';
-        let current = this.head;
-        while( current !== null){
-            str += current.value + ' -> ';
-            current = current.next;
-        }
-        console.log(str);
-    }
-
-    contains(target) {
-        let current = this.head;
-        while( current !== null){
-            if (current.value === target){
-                return true
-            }
-            current = current.next;
-        }
-        return false
-    }
-
 }
+
+
+
+
+
+
 
 
 const list = new LinkedList();
 list.append('a');
 list.append('b');
 list.append('c');
-list.append(true);
-list.print();
-
-console.log(list.contains('a'));
-console.log(list.contains('b'));
-console.log(list.contains('c'));
-console.log(list.contains(true));
-console.log(list.contains('x'));
-console.log(list.contains('z'));
+console.log(list.head)
