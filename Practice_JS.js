9d01f324e3054d02788ea7c98a8e0ca2ffc69c20
Azachary8232 <azachary8232@hotmail.com>
2022-05-23 11:10:15 -0600

    Updated

--- Practice_JS.js
+++ Practice_JS.js
@@ -12,7 +12,7 @@ class LinkedList{
         this.head = null
     }
 
-    append(){
+    append(value){
         if(this.head === null){
             this.head = new Node(value)
             return
@@ -21,7 +21,28 @@ class LinkedList{
         while(current.next !== null){
             current = current.next
         }
-        current.next = new Node(value)
-        
+        current.next = new Node(value) 
+        return
+    }
+
+    print(){
+        let str = ""
+        if(this.head = null){
+            return "empty list"
+        }
+        current = this.head
+        while(current.next !== null){
+            str += " -> " + current.value
+        }
+        str += current.value
     }
 }
+
+
+list = new LinkedList()
+list.append(5)
+list.append(1)
+list.append(3)
+list.append(8)
+
+console.log(list)
