194acb00cfab2225a51b5b0282f4488da8134115
Azachary8232 <azachary8232@hotmail.com>
2022-05-23 11:04:51 -0600

    Updated

--- Practice_JS.js
+++ Practice_JS.js
@@ -1,51 +1,27 @@
 
 
-
 class Node{
     constructor(value){
-        this.value = value;
+        this.value = value
         this.next = null
     }
 }
 
-
 class LinkedList{
     constructor(){
-        this.head = null;
+        this.head = null
     }
 
-    append(value){
+    append(){
         if(this.head === null){
-            this.head = new Node(value);
+            this.head = new Node(value)
             return
         }
         let current = this.head
         while(current.next !== null){
-            current = current.next;
+            current = current.next
         }
-        current.next = new Node(value);
+        current.next = new Node(value)
+        
     }
-
-    print(){
-        let str = '';
-        let current = this.head;
-        while( current !== null){
-            str += current.value + ' -> ';
-            current = current.next;
-        }
-        console.log(str);
-    }
-
-
-
 }
-
-
-const list = new LinkedList();
-
-list.append(8);
-list.append('dog');
-list.append(true);
-list.append('45');
-
-list.print();
