d5fb69b58f79cfd35ff4860bcb9d240bf4115ea1
Azachary8232 <azachary8232@hotmail.com>
2022-06-02 08:45:33 -0600

    Updated

---
+++ ApplesAndOranges.js
@@ -0,0 +1,18 @@
+
+
+
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    console.log(s);
+    console.log(t);
+    console.log(a);
+    console.log(b);
+    console.log(apples);
+    console.log(oranges);
+
+}
+
+
+
+
+console.log(countApplesAndOranges(7,11,5,15,[-2,2,1],[5,-6]));
