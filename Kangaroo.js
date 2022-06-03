e26894a4987779a133ab05edfec7a8fc52bea641
Azachary8232 <azachary8232@hotmail.com>
2022-06-03 14:28:37 -0600

    Updated

--- Kangaroo.js
+++ Kangaroo.js
@@ -1,12 +1,40 @@
 
+//write a loop for each jump
+//jumps are unknown so a while loop
+//each loop add jump distance from starting point
+// two variable for current location of each kangaroo
 
+// fail break will be when kangaroo with longer jump is ahead of kangaroo with smaller jump
+    // need to check for kangaroo with longer jump
+
+//final check will be if both kangaroos are on the same spot
 
 
 function kangaroo(x1, v1, x2, v2){
-    console.log(x1);
-    console.log(v1);
-    console.log(x2);
-    console.log(v2);
+    let positionK1 = x1;
+    let positionK2 = x2;
+    let longestJumper;
+
+    if( v1 > v2){
+        longestJumper = v1;
+    }
+    else{
+        longestJumper = v2;
+    }
+
+    let jump = 0;
+
+    while( (longestJumper == v1 && x1 < x2) || (longestJumper == v2 && x2 < x1)){
+        console.log("running");
+        positionK1 += v1;
+        positionK2 += v2;
+
+        if(positionK1 == positionK2){
+            return "yes";
+        }
+
+    }
+
 }
 
-kangaroo(0, 3, 4, 2);
+console.log(kangaroo(0, 3, 4, 2));
