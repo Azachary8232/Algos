fb531b00345bcb27f4e3aba350e4318643d8641f
Azachary8232 <azachary8232@hotmail.com>
2022-06-07 12:38:34 -0600

    Updated

--- JavaScript/queensAttack.js
+++ JavaScript/queensAttack.js
@@ -1,4 +1,7 @@
 
 // create a game board
-// place queen on board
+// place queen on board; give it a 'q' value
 // place obstacles on board; give them an 'o' value
+// place queens moves on the board if not occupied by an 'o'; give them an 'm' value
+// count all 'm' values
+
