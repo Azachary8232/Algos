d4c460f0de76c9a8ca848783f1ce2623e6181da0
Azachary8232 <azachary8232@hotmail.com>
2022-06-08 16:55:25 -0600

    Updated

--- JavaScript/queensAttack.js
+++ JavaScript/queensAttack.js
@@ -24,36 +24,60 @@ function queensAttack(n, k, r_q, c_q, obstacles){
         gameBoard[x[0]-1][x[1]-1] = "o";
     }
     // queen left
-    let row = r_q - 1 - 1;
+    let lRowU = r_q - 1 - 1;
+    let lRowD = r_q;
     for( let col = c_q - 1 - 1; col >= 0; col--){
         if( gameBoard[r_q - 1][col] !== "o"){
             gameBoard[r_q - 1][col] = "m";
         } else{
             break
         }
-        if(row >= 0 ){
-            if(gameBoard[row][col] !== "o") {
-                gameBoard[row][col] = "l"
-                console.log(row);
-                row --
+        if(lRowU >= 0 ){
+            if(gameBoard[lRowU][col] !== "o") {
+                gameBoard[lRowU][col] = "m"
+                lRowU --
+            } else {
+                continue
+            }
+        }
+        if(lRowD < n ){
+            if(gameBoard[lRowD][col] !== "o") {
+                gameBoard[lRowD][col] = "m"
+                lRowD ++
             } else {
                 continue
             }
         }
     }
-    for( x = c_q; x < n; x++){
-        if( gameBoard[r_q - 1][x] !== "o"){
-            gameBoard[r_q - 1][x] = "m"
+    // queen right
+    let rRowU = r_q - 1 - 1;
+    let rRowD = r_q;
+    for( let col = c_q; col < n; col++){
+        if( gameBoard[r_q - 1][col] !== "o"){
+            gameBoard[r_q - 1][col] = "m"
         } else{
             break
         }
-        // if( gameBoard[x][x] !== "o"){
-        //     gameBoard[x][x] = "r"
-        // }
+        if(rRowU >= 0 ){
+            if(gameBoard[rRowU][col] !== "o") {
+                gameBoard[rRowU][col] = "m"
+                rRowU --
+            } else {
+                continue
+            }
+        }
+        if(rRowD < n ){
+            if(gameBoard[rRowD][col] !== "o") {
+                gameBoard[rRowD][col] = "m"
+                rRowD ++
+            } else {
+                continue
+            }
+        }
     }
 
     // queen up
-    for( x = r_q -1 - 1; x >= 0; x--){
+    for( let x = r_q -1 - 1; x >= 0; x--){
         let y = 0
         if( gameBoard[x][c_q - 1] !== "o"){
             gameBoard[x][c_q - 1] = "m"
@@ -68,17 +92,21 @@ function queensAttack(n, k, r_q, c_q, obstacles){
         } else{
             break
         }
-        // if( gameBoard[x][x] !== "o"){
-        //     gameBoard[x][x] = "m"
-        // }
+    }
+    let count = 0
+    for(let a = 0; a < n; a++){
+        for(let b = 0; b < n; b++){
+            if (gameBoard[a][b] === "m"){
+                count++;
+            }
+        }
     }
 
-
-    gameBoard.forEach(function(row) {
-        console.log(row.join('|'));
-    });
-    console.log(gameBoard);
+    // gameBoard.forEach(function(row) {
+    //     console.log(row.join('|'));
+    // });
+    console.log(count);
 
 }
 
-queensAttack(4, 0, 2, 3, [[4,1]]);
+queensAttack(8, 1, 4, 4, [[3,5]]);
